bff3073b0f13f122d2bf789eb0feca813c9cd114
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2021-12-21 11:32:55 +0100

    change wmts

--- connect/MyDriveLoggedIn.py
+++ connect/MyDriveLoggedIn.py
@@ -363,15 +363,15 @@ class MyDriveLoggedInTab(QDialog):
             theurl = F"{URL}/wmts/{mapid}/{self.loginToken}"
             actualurl = f"tileMatrixSet=matrix_{self.currentZoom}&crs=EPSG:3857&layers={ids}&styles=&format=image/png&url={theurl}"
             log(actualurl)
-            rlayer = QgsRasterLayer(actualurl, f"{self.currentTimestamp['dateTo']}_{itemdata['name']}", 'WMS')
+            #rlayer = QgsRasterLayer(actualurl, f"{self.currentTimestamp['dateTo']}_{itemdata['name']}", 'WMS')
             
-            # iface.addRasterLayer(actualurl, f"{self.currentTimestamp['dateTo']}_{itemdata['name']}", 'wms')
+            iface.addRasterLayer(actualurl, f"{self.currentTimestamp['dateTo']}_{itemdata['name']}", 'wms')
 
-            if not rlayer.isValid():
-                displayMessageBox("Error loading layer", "Layer failed to load.")
-                log("Layer failed to load!")
-            else:
-               QgsProject.instance().addMapLayer(rlayer)
+            # if not rlayer.isValid():
+            #     displayMessageBox("Error loading layer", "Layer failed to load.")
+            #     log("Layer failed to load!")
+            # else:
+            #    QgsProject.instance().addMapLayer(rlayer)
             # same as above
             self.currentItem = self.previousItem
 
